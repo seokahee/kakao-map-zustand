a99ccad2d48cd4d8480b8ca5c28c72384bac0aa8
seokahee <kahee430@gmail.com>
2024-11-28 18:46:38 +0900

    fix : 컴포넌트 렌더 시 바텀시트 최초 높이 설정 완료

--- src/bottomSheet/BottomSheet.tsx
+++ src/bottomSheet/BottomSheet.tsx
@@ -1,26 +1,21 @@
 import { motion } from "framer-motion";
 import BottomSheetContents from "./BottomSheetContents";
 import BottomSheetHeader from "./BottomSheetHeader";
-import { useEffect, useState } from "react";
 
 function BottomSheet() {
-  const [dragConstraints, setDragConstraints] = useState({ top: 0, bottom: 0 });
-
-  // 컴포넌트 마운트 시 드래그 제약조건 실행
-  useEffect(() => {
-    // 드래그 가능한 범위(제약조건) 설정 함수
-
-    const updataConstraint = () => {
-      setDragConstraints({ top: 10, bottom: window.innerHeight - 50 });
-    };
-  }, []);
-
   return (
     <motion.div
       className="sheet-wrap"
       drag="y"
       dragMomentum={false}
-      dragConstraints={{ top: 50, bottom: window.innerHeight - 10 }}
+      dragConstraints={{
+        top: 50,
+        bottom: window.innerHeight - 50,
+      }} // 위에서 50px 떨어질때까지, 아래 기준 스크린 화면 -50px까지
+      initial={{
+        y: window.innerHeight - 50, // 렌더 시 바텀시트 위치 지점
+        // opacity: 0,
+      }}
     >
       <BottomSheetHeader />
       <div className="sheet-contents-wrap">
