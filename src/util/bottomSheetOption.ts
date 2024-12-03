ce04bc3dc32a4ce138ca59aad4e5c60aaeac0c8a
seokahee <kahee430@gmail.com>
2024-12-03 14:20:36 +0900

    바텀시트 동적 좌표 설정, 선택 영역에따라 좌표 설정, 모던시트 통합본

--- src/util/bottomSheetOption.ts
+++ src/util/bottomSheetOption.ts
@@ -1,7 +1,12 @@
-export const MIN_Y = window.innerHeight * 0.1;
-export const MAX_Y = window.innerHeight * 1 - 50;
-export const MAX_Y_SHEET_HEIGHT = window.innerHeight - MIN_Y;
+// export const MIN_Y = window.innerHeight * 0.1;
+// export const MAX_Y = window.innerHeight * 1 - 50;
+// export const MAX_Y_SHEET_HEIGHT = window.innerHeight - MIN_Y;
 
-// MIN_Y 바텀 시트가 최대로 올라갔을 때 Y좌표 값
-// MAX_Y 바컴 시트가 최대로 내려갔을 때 Y좌표 값
-// MAX_Y_SHEET_HEIGHT 바텀시트 높이
+// export const MIN_Y = 60;
+// export const MAX_Y = window.innerHeight - 160;
+// export const BOTTOM_SHEET_HEIGHT = window.innerHeight - MIN_Y;
+
+// // MIN_Y 바텀 시트가 최대로 올라갔을 때 Y좌표 값
+// // MAX_Y 바컴 시트가 최대로 내려갔을 때 Y좌표 값
+// // MAX_Y_SHEET_HEIGHT 바텀시트 높이
+export {};
