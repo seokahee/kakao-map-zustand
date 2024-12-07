83223d50bcf466f47004923212037c62239be66d
서가희 <kahee430@gmail.com>
2024-12-07 23:07:43 +0900

    fix : 바텀시트 3/1 이상 접히면 원위치, 화면비 헤더 위치 고정

--- src/temp/bottomSheet/BottomSheetTest.tsx
+++ src/temp/bottomSheet/BottomSheetTest.tsx
@@ -3,30 +3,25 @@ import { miniData } from "../data";
 
 function BottomSheetTest() {
   const [testState] = useState(miniData);
-  const [initial, setInitial] = useState(window.innerHeight * 0.5); // 바텀시트 초기값
-
-  console.log("initial", initial);
-
-  const [isDragging, setIsDragging] = useState(false);
+  const [initial, setInitial] = useState(window.innerHeight - 50); // 바텀시트 초기값
+  const [isDragging, setIsDragging] = useState(false); // 드래그 여부 확인
 
   const startY = useRef(0); // 드래그 시작 위치 설정
-  const sheetPosition = useRef(initial);
+  const sheetPosition = useRef(initial); // 바텀시트 이전 값 저장
 
-  const minPosition = window.innerHeight * 0.1; // 상단 제한
+  const minPosition = useRef(window.innerHeight * 0.1); // 상단 제한
   const maxPosition = useRef(window.innerHeight - 50); // 하단 제한
 
   // 화면 크기 변경 감지
   useEffect(() => {
-    const handleResize = () => {
-      maxPosition.current = window.innerHeight - 50; // 하단 제한 동적 갱신
-      if (initial > maxPosition.current) {
-        setInitial(maxPosition.current); // 현재 위치 보정
-      }
+    const updatePositions = () => {
+      setInitial(window.innerHeight - 50);
     };
 
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, [initial]);
+    // 초기 화면 크기 및 크기 변경 시 갱신
+    updatePositions();
+    window.addEventListener("resize", updatePositions);
+  }, []);
 
   // 마우스 클릭 또는 터치
   const handleMouseDown = (e: any) => {
@@ -39,7 +34,6 @@ function BottomSheetTest() {
   const handleMouseMove = (e: any) => {
     if (!isDragging) return;
     const currentY = e.clientY || e.touches?.[0].clientY; // 이동 시 바텀시트 위치 추적
-
     // const delta = currentY - startY.current; // 이동 후 마우스 위치 - 드래그 시작 위치 = 이동거리
     // const newPosition = sheetPosition.current + delta; // 바텀시트 시작 위치 + 이동거리
 
@@ -48,9 +42,8 @@ function BottomSheetTest() {
     // 현재 바텀시트에 마우스 이동거리를 더해 바텀시트 위치 계산
 
     setInitial(
-      Math.min(Math.max(newPosition, minPosition), maxPosition.current)
+      Math.min(Math.max(newPosition, minPosition.current), maxPosition.current)
     );
-
     // 바텀 시트 위치 제한을 위해 Math.max(newPosition, minPosition) 바텀시트 현재 위치 , 상단 위치 비교 후 큰 값 반환
     // Math.min(Max 값, maxPosition.current) 반환된 값과 바텀시트 하단 값 비교 후 작은 값 반환
   };
@@ -59,7 +52,11 @@ function BottomSheetTest() {
   const handleMouseUp = () => {
     if (isDragging) {
       setIsDragging(false);
-      sheetPosition.current = initial; // 현재 바텀시트 위치 저장
+
+      // 바텀시트가 3/1 이상 접히면 초기 위치로 이동, 아니면 현재 위치 유지
+      setInitial(
+        initial > window.innerHeight * (2 / 3) ? maxPosition.current : initial
+      );
     }
   };
 
